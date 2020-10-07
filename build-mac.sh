547c85692023c598c7fcf3c7325b39fe23d92b32
thib1984 <45128847+thib1984@users.noreply.github.com>
2020-10-07 19:57:23 +0200

    Add files via upload

---
+++ build-mac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+cd src
+echo "build in progress ..."
+echo "GOPATH : " $GOPATH
+echo "GOBIN : " $GOBIN
+go mod download
+go get -d -v
+GO111MODULE=on CGO_ENABLED=0 GOOS=linux go build -o git-pull .
+
+echo "build terminated, application build in $GOBIN path."
